ab099700e08126aedb0fa365ae797403cf5b6d9d
olawale-o <omoogunolawale@gmail.com>
2022-01-19 23:00:00 +0100

    Add styling

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
+import router from "./routes";
+import "./assets/index.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
