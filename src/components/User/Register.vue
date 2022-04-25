c0784eb1d418ef982db9985da7b7996e45827027
olawale-o <omoogunolawale@gmail.com>
2022-04-25 20:14:38 +0100

    User can register

--- src/components/User/Register.vue
+++ src/components/User/Register.vue
@@ -1,44 +1,94 @@
 <template>
   <div class="signup-container">
-    <form>
-      <h1>Getting Started</h1>
-      <div class="switch-buttons">
-        <button
-          type="button"
-          class="switch-btn"
-          :class="{ active: !isFocus }"
-          @click="onActive"
-        >
-          Login
-        </button>
-        <button
-          type="button"
-          class="switch-btn"
-          :class="{ active: isFocus }"
-          @click="onActive"
-        >
-          Register
-        </button>
-      </div>
-      <div class="continue-with">
-        <button type="button" class="google">Google</button>
-        <button type="button" class="facebook">Facebook</button>
-      </div>
-      <div class="or">
-        <span class="left" />
-        <span class="text">or</span>
-        <span class="right" />
+    <Form
+      @submit="onSubmit"
+      :validation-schema="registerSchema"
+      :initial-values="registerInitialValues"
+      v-slot="{ isSubmitting, meta: { dirty, valid } }"
+    >
+      <AuthFormHeader :title="title" :is-focus="isFocus" @on-reset="onReset" />
+      <div class="field">
+        <Field
+          :name="username.name"
+          type="text"
+          class="input"
+          placeholder="Username"
+        />
+        <ErrorMessage name="username" class="error" />
       </div>
       <div class="field">
-        <input type="text" placeholder="Username" class="input" />
+        <Field
+          :name="registerEmail.name"
+          type="email"
+          class="input"
+          placeholder="Email"
+        />
+        <ErrorMessage name="email" class="error" />
       </div>
       <div class="field">
-        <input type="text" placeholder="Email" class="input" />
+        <Field
+          :name="registerPassword.name"
+          type="password"
+          class="input"
+          placeholder="Password"
+        />
+        <ErrorMessage name="password" class="error" />
       </div>
-      <div class="field">
-        <input type="text" placeholder="Password" class="input" />
-      </div>
-      <button type="button" class="auth-btn">Create</button>
-    </form>
+      <button
+        type="submit"
+        class="button button-primary"
+        :disabled="isSubmitting || !(dirty && valid)"
+      >
+        Create
+      </button>
+    </Form>
   </div>
 </template>
+
+<script>
+import { Form, ErrorMessage, Field } from "vee-validate";
+import { AuthFormHeader } from "@/components/Shared";
+import { authSchema, authInitialValues, authModel } from "@/forms/Auth";
+export default {
+  name: "RegisterComponent",
+  emits: ["onActive", "onRegister"],
+  props: {
+    isFocus: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  components: {
+    AuthFormHeader,
+    Field,
+    ErrorMessage,
+    Form,
+  },
+  setup(props, { emit }) {
+    const {
+      register: {
+        formField: { registerEmail, registerPassword, username },
+      },
+    } = authModel;
+    const { registerSchema } = authSchema;
+    const { registerInitialValues } = authInitialValues;
+    return {
+      title: "Getting Started",
+      onLogin: () => {
+        emit("onRegister");
+      },
+      onReset: () => {
+        emit("onActive");
+      },
+      onSubmit: ({ username, email, password }) => {
+        emit('onRegister', { user: { username, email, password } });
+      },
+      registerEmail,
+      registerPassword,
+      username,
+      registerSchema,
+      registerInitialValues,
+    };
+  },
+};
+</script>
