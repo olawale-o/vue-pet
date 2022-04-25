374be495593f3e89b425459de00f5fa258d388d8
olawale-o <omoogunolawale@gmail.com>
2022-04-25 17:52:36 +0100

    Fix linters

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,87 +2,16 @@
   <div class="auth-container">
     <div class="container">
       <div class="form-container" :class="{ slide: isFocus }">
-        <div class="login-container">
-          <form>
-            <h1>Welcome back</h1>
-            <div class="switch-buttons">
-              <button
-                type="button"
-                class="switch-btn"
-                :class="{ active: !isFocus }"
-                @click="onActive"
-              >
-                Login
-              </button>
-              <button
-                type="button"
-                class="switch-btn"
-                :class="{ active: isFocus }"
-                @click="onActive"
-              >
-                Register
-              </button>
-            </div>
-            <div class="continue-with">
-              <button type="button" class="google">Google</button>
-              <button type="button" class="facebook">Facebook</button>
-            </div>
-            <div class="or">
-              <span class="left"></span>
-              <span class="text">or</span>
-              <span class="right"></span>
-            </div>
-            <div class="field">
-              <input type="text" placeholder="Email" class="input" />
-            </div>
-            <div class="field">
-              <input type="text" placeholder="Password" class="input" />
-            </div>
-            <button type="button" class="auth-btn">Log in</button>
-          </form>
-        </div>
-        <div class="signup-container">
-          <form>
-            <h1>Getting Started</h1>
-            <div class="switch-buttons">
-              <button
-                type="button"
-                class="switch-btn"
-                :class="{ active: !isFocus }"
-                @click="onActive"
-              >
-                Login
-              </button>
-              <button
-                type="button"
-                class="switch-btn"
-                :class="{ active: isFocus }"
-                @click="onActive"
-              >
-                Register
-              </button>
-            </div>
-            <div class="continue-with">
-              <button type="button" class="google">Google</button>
-              <button type="button" class="facebook">Facebook</button>
-            </div>
-            <div class="or">
-              <span class="left" />
-              <span class="text">or</span>
-              <span class="right" />
-            </div>
-            <div class="field">
-              <input type="text" placeholder="Username" class="input" />
-            </div>
-            <div class="field">
-              <input type="text" placeholder="Email" class="input" />
-            </div>
-            <div class="field">
-              <input type="text" placeholder="Password" class="input" />
-            </div>
-            <button type="button" class="auth-btn">Create</button>
-          </form>
-        </div>
+        <LoginComponent
+          :is-focus="isFocus"
+          @on-active="onActive"
+          @on-login="onLogin"
+        />
+        <RegisterComponent
+          :is-focus="isFocus"
+          @on-active="onActive"
+          @on-register="onRegister"
+        />
       </div>
       <div class="overlay-container"></div>
     </div>
@@ -91,17 +20,33 @@
 
 <script>
 import { ref } from "vue";
+import LoginComponent from "@/components/User/Login";
+import RegisterComponent from "@/components/User/Register";
 export default {
   name: "user",
+  components: {
+    LoginComponent,
+    RegisterComponent,
+  },
   setup() {
     const isFocus = ref(false);
     const onActive = () => {
-      console.log("onActive");
       isFocus.value = !isFocus.value;
     };
+
+    const onLogin = async (values) => {
+      console.log(values);
+    };
+
+    const onRegister = async (values) => {
+      console.log(values);
+    };
+
     return {
       isFocus,
       onActive,
+      onLogin,
+      onRegister,
     };
   },
 };
