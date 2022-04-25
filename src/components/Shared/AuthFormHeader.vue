46b790d6f6660bbf2da4c053fdc6bbd40ed56936
olawale-o <omoogunolawale@gmail.com>
2022-04-25 18:09:03 +0100

    Create form header for login and signup form

---
+++ src/components/Shared/AuthFormHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div class="switch-buttons">
+    <button
+      type="button"
+      class="button button-outline"
+      :disabled="!isFocus"
+      @click="$emit('onReset')"
+    >
+      Login
+    </button>
+    <button
+      type="button"
+      class="button button-outline"
+      :disabled="isFocus"
+      @click="$emit('onReset')"
+    >
+      Register
+    </button>
+  </div>
+  <div class="continue-with">
+    <button type="button" class="button button-social google w-100">
+      Google
+    </button>
+    <button type="button" class="button button-social facebook w-100">
+      Facebook
+    </button>
+  </div>
+  <div class="or">
+    <span class="left" />
+    <span class="text">or</span>
+    <span class="right" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthFormHeader",
+  props: {
+    title: {
+      type: String,
+    },
+    isFocus: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {},
+};
+</script>
