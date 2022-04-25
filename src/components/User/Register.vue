5c3f97e66240eb6574000ebeda8e778f9878a4f9
olawale-o <omoogunolawale@gmail.com>
2022-04-25 12:19:29 +0100

    Create Register component

---
+++ src/components/User/Register.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="signup-container">
+    <form>
+      <h1>Getting Started</h1>
+      <div class="switch-buttons">
+        <button
+          type="button"
+          class="switch-btn"
+          :class="{ active: !isFocus }"
+          @click="onActive"
+        >
+          Login
+        </button>
+        <button
+          type="button"
+          class="switch-btn"
+          :class="{ active: isFocus }"
+          @click="onActive"
+        >
+          Register
+        </button>
+      </div>
+      <div class="continue-with">
+        <button type="button" class="google">Google</button>
+        <button type="button" class="facebook">Facebook</button>
+      </div>
+      <div class="or">
+        <span class="left" />
+        <span class="text">or</span>
+        <span class="right" />
+      </div>
+      <div class="field">
+        <input type="text" placeholder="Username" class="input" />
+      </div>
+      <div class="field">
+        <input type="text" placeholder="Email" class="input" />
+      </div>
+      <div class="field">
+        <input type="text" placeholder="Password" class="input" />
+      </div>
+      <button type="button" class="auth-btn">Create</button>
+    </form>
+  </div>
+</template>
