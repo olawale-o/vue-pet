66cfbd9c36efe7ca732b26b86dc035cd679fb9bb
olawale-o <omoogunolawale@gmail.com>
2022-04-25 20:18:15 +0100

    Create wrapper for post and get http verbs

---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import BASE_URI from "../constants";
+
+const api = axios.create({
+  baseURL: BASE_URI,
+});
+
+export const post = async (uri, options = {}) => {
+  const { body = {} } = options;
+  const response = await api.post(uri, body);
+  return response;
+};
+
+export const get = async (uri) => {
+  const response = await api.get(uri);
+  return response;
+};
+
+export default api;
