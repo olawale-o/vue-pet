09bf08a4047ddbe6ee4c961c76355b28f607567d
olawale-o <omoogunolawale@gmail.com>
2022-04-25 20:20:16 +0100

    Create constants

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import BASE_URI from "../constants";
+import BASE_URI from "@/constants";
 
 const api = axios.create({
   baseURL: BASE_URI,
---
+++ src/constants/petColors.js
@@ -0,0 +1,21 @@
+const petColors = [
+  {
+    id: 1,
+    name: 'black',
+  },
+  {
+    id: 2,
+    name: 'brown',
+  },
+  {
+    id: 3,
+    name: 'orange',
+  },
+  {
+    id: 4,
+    name: 'white',
+  },
+];
+  
+export default petColors;
+  
