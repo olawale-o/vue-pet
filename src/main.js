9f6353ef5fa3caba90888b3d59e8b9bd131a6522
olawale-o <omoogunolawale@gmail.com>
2022-04-26 03:24:16 +0100

    Intercept request and response

--- src/main.js
+++ src/main.js
@@ -2,7 +2,26 @@ import { createApp } from "vue";
 import router from "./routes";
 import "./assets/index.css";
 import App from "./App.vue";
+import pinia from "./store";
+import api from "./api";
+import useRootStore from "./store/root";
 
+api.interceptors.request.use((config) => {
+  const rootStore = useRootStore();
+  const token = rootStore.currentToken;
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
+  
+api.interceptors.response.use(function(response) {
+  return response;
+}, function(error) {
+  throw error;
+});
+  
 const app = createApp(App);
+app.use(pinia);
 app.use(router);
 app.mount("#app");
