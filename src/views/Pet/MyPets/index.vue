4662f8535d89e0c6498dabd6b465ee8dcb68463d
olawale-o <omoogunolawale@gmail.com>
2022-04-27 00:37:03 +0100

    Create Pet view

---
+++ src/views/Pet/MyPets/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <ProfileArea :pets="myPets" />
+</template>
+
+<script>
+import { storeToRefs } from "pinia";
+import usePetStore from "@/store/pet";
+import { ProfileArea } from "@/components";
+export default {
+  name: "MyPets",
+  components: {
+    ProfileArea,
+  },
+  setup() {
+    const { myPets, loading } = storeToRefs(usePetStore());
+    return {
+      myPets,
+      loading,
+    };
+  },
+};
+</script>
---
+++ src/views/Pet/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: "PetView",
+  setup() {},
+};
+</script>
+
+<style scoped></style>
