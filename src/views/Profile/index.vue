02d8d521c66e013d2edd39b500f993fb6b0d5cf0
olawale-o <omoogunolawale@gmail.com>
2022-04-26 04:13:11 +0100

    Create profile view

---
+++ src/views/Profile/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="profile">
+    <div class="content">
+      <div class="main">
+        <ProfileSideBar :userId="user.id" />
+        <div class="outlet">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { storeToRefs } from 'pinia';
+import useAuthUserStore from '@/store/authUserStore';
+export default {
+  name: "ProfileView",
+  components: {
+    ProfileSideBar,
+  },
+  setup() {
+    const { user } = storeToRefs(useAuthUserStore());
+    return {
+      user,
+    }
+  },
+}
+</script>
+
+<style scoped>
+  @import './style.css';
+</style>
