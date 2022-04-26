7337a3616b822595fc7f229f319d780c439213b0
olawale-o <omoogunolawale@gmail.com>
2022-04-26 13:48:15 +0100

    Use navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="app">
+    <Navbar />
     <router-view />
   </div>
 </template>
 <script>
+import Navbar from "./components/Navbar";
 export default {
   name: "App",
+  components: {
+    Navbar,
+  },
 };
 </script>
 <style>
