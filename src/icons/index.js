5a85e3cdd47043ac0c00afcda9b00817da8b3989
olawale-o <omoogunolawale@gmail.com>
2022-04-26 22:42:36 +0100

    Export fontawesome icons

---
+++ src/icons/index.js
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faEllipsisH, faHeart, faTag } from "@fortawesome/free-solid-svg-icons";
+library.add(faEllipsisH, faHeart, faTag);
+
+export default FontAwesomeIcon;
