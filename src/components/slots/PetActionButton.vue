643130c92bf05a4dc21569025c82045d2098d16e
olawale-o <omoogunolawale@gmail.com>
2022-04-26 16:18:13 +0100

    Create PetActionButton slot

---
+++ src/components/slots/PetActionButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    type="button"
+    className="action__btn"
+    @click="$emit('onAction')"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  emits: ["onAction"],
+  setup() {},
+}
+</script>
---
+++ src/components/slots/index.js
@@ -0,0 +1,3 @@
+export { default as ToolTipItem } from "./ToolTipItem";
+export { default as ToolTipPopUp } from "./ToolTipPopUp";
+export { default as PetActionButton } from "./PetActionButton";
