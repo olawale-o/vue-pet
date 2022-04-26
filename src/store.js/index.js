a45ad3b848a5a51870812f9fff84caea24d7020e
olawale-o <omoogunolawale@gmail.com>
2022-04-26 01:10:21 +0100

    Define root store

--- src/store.js/index.js
+++ src/store.js/index.js
@@ -1,5 +1,5 @@
-import { createPinia } from 'pinia';
-import piniaPersist from 'pinia-plugin-persist';
+import { createPinia } from "pinia";
+import piniaPersist from "pinia-plugin-persist";
 
 const pinia = createPinia();
 pinia.use(piniaPersist);
