c2fa7c5a4d2bf1ed2a69fd3e8a5eb2e3749b8606
olawale-o <omoogunolawale@gmail.com>
2022-04-27 02:47:59 +0100

    Inject modal

--- src/components/MyPetCard/index.vue
+++ src/components/MyPetCard/index.vue
@@ -39,6 +39,7 @@
 </template>
 
 <script>
+import { inject } from "vue";
 import secondDog from "@/assets/images/dog2.jpg";
 import { PetToolTipPopUp } from "@/components/Shared";
 import { titlelize, GENDER_ENUM } from "@/helper";
@@ -57,16 +58,14 @@ export default {
       type: Number,
       required: true,
     },
-    modal: {
-      type: Boolean,
-      required: true,
-    },
   },
   setup() {
+    const { modal } = inject("global");
     return {
       secondDog,
       titlelize,
       GENDER_ENUM,
+      modal,
     };
   },
 };
