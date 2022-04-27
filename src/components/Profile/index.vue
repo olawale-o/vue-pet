ab23ab5a641368003ae9c0d01c727801aad13590
olawale-o <omoogunolawale@gmail.com>
2022-04-27 04:15:32 +0100

    Create profile component

---
+++ src/components/Profile/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div className="profile__area">
+    <div className="pets">
+      <MyPetCard
+        v-for="pet in pets"
+        :key="pet.id"
+        :pet="pet"
+        :choosenPet="state.choosePet"
+        @set-pet="onChoosePet"
+        :modal="state.modal"
+      />
+    </div>
+    <Teleport to="#overlay">
+      <div v-if="state.overlay" className="overlay" @click="close" />
+    </Teleport>
+  </div>
+</template>
+
+<script>
+import { reactive, provide } from "vue";
+import MyPetCard from "@/components/MyPetCard";
+export default {
+  name: "ProfileArea",
+  components: {
+    MyPetCard,
+  },
+  props: {
+    pets: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {
+    const state = reactive({
+      choosePet: 0,
+      index: 0,
+      modal: false,
+      overlay: false,
+    });
+
+    const onChoosePet = (petId) => {
+      state.choosePet = petId;
+      state.index = petId;
+      state.overlay = !state.overlay;
+    };
+
+    const close = () => {
+      state.overlay = !state.overlay;
+      state.choosePet = 0;
+    };
+
+    const openModal = () => {
+      state.overlay = !state.overlay;
+      state.modal = !state.modal;
+      state.choosePet = 0;
+    };
+
+    provide("edit", {
+      openModal,
+    });
+
+    return {
+      state,
+      onChoosePet,
+      close,
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import "./style.css";
+</style>
