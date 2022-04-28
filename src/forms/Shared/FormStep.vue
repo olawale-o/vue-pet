b345f4dd7ed223438cfc52fcd7b926411cd645d0
olawale-o <omoogunolawale@gmail.com>
2022-04-28 17:08:16 +0100

    Create form step component

---
+++ src/forms/Shared/FormStep.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="shouldShow">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { inject, computed} from "vue";
+export default {
+  name: "FormStep",
+  setup() {
+    const currentIdx = inject("STEP_COUNTER").value++;
+    const formStepIdx = inject("CURRENT_STEP_INDEX");
+
+    const shouldShow = computed(() => {
+      return currentIdx === formStepIdx.value;
+    });
+
+    return {
+      shouldShow,
+    };
+  },
+};
+</script>
---
+++ src/forms/Shared/index.js
@@ -0,0 +1,3 @@
+export { default as FormWizard } from "./FormWizard";
+export { default as FormStep } from "./FormStep";
+export { default as CustomInput } from "./CustomInput";
