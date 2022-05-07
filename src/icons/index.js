abb6f7d4737219e45620cea957a8942b638d5113
olawale-o <omoogunolawale@gmail.com>
2022-05-07 07:22:22 +0100

    Add more icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,6 +1,6 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faEllipsisH, faHeart, faTag } from "@fortawesome/free-solid-svg-icons";
-library.add(faEllipsisH, faHeart, faTag);
+import { faEllipsisH, faHeart, faTag, faBars } from "@fortawesome/free-solid-svg-icons";
+library.add(faEllipsisH, faHeart, faTag, faBars);
 
 export default FontAwesomeIcon;
