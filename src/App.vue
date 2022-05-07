462a56bb61c8108bd979008fa7c19e586a1aa085
olawale-o <omoogunolawale@gmail.com>
2022-05-07 10:58:16 +0100

    Get breeds on app mount

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,27 @@
   </div>
 </template>
 <script>
-import { provide } from "vue";
+import { provide, onMounted } from "vue";
 import { storeToRefs } from "pinia";
 import useUserAuthStore from "./store/auth";
+import usePetStore from "./store/pet";
 import Navbar from "./components/Navbar";
+import { getDogBreedsService } from "./services";
 export default {
   name: "App",
   components: {
     Navbar,
   },
   setup() {
+    const petStore = usePetStore();
     const { user } = storeToRefs(useUserAuthStore());
+    const { breeds } = storeToRefs(usePetStore());
     provide("global", {
       user,
+      breeds,
     });
+
+    onMounted(() => petStore.getBreeds(getDogBreedsService));
   },
 };
 </script>
