a8a3ab64cb27a3d263a46761846d994eaa767306
olawale-o <omoogunolawale@gmail.com>
2022-05-07 13:58:58 +0100

    Create pet photos view

---
+++ src/components/Pet/PhotoList/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="photo__list">
+    <div className="photo" :key="index" v-for="(photo, index) in photos">
+      <img :src="`${BASE_URI}${photo.url}`" alt="dog" />
+      <button type="button" className="photo__btn">
+        <font-awesome-icon icon="pen" class="icon" color="#fff" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import BASE_URI from "@/constants";
+export default {
+  name: "PhotoList",
+  props: {
+    photos: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {
+    return {
+      BASE_URI,
+    };
+  },
+}
+</script>
+<style scoped>
+@import "./style.css";
+</style>
---
+++ src/views/Pet/Photos/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <PhotoList :photos="photos" />
+</template>
+
+<script>
+import { storeToRefs } from "pinia";
+import usePetStore from "@/store/pet";
+import PhotoList from "@/components/Pet/PhotoList";
+export default {
+  name: "PhotosView",
+  components: {
+    PhotoList,
+  },
+  setup() {
+    const { photos } = storeToRefs(usePetStore());
+    return {
+      photos,
+    };
+  },
+};
+</script>
