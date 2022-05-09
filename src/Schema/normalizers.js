2bf48af9a97a201e127be9fe784d1706b6f3d0a6
olawale-o <omoogunolawale@gmail.com>
2022-05-09 01:52:03 +0100

    Normalize photos

--- src/Schema/normalizers.js
+++ src/Schema/normalizers.js
@@ -6,3 +6,8 @@ export const normalizeMyPets = (data) => {
   const { entities: { dogs: pets, photos }, result } = normalize(data, [petSchema]);
   return { pets, photos, petIds: result };
 };
+
+export const normalizePhotos = (data) => {
+  const { entities: { photos } } = normalize(data, [photoSchema]);
+  return photos;
+};
