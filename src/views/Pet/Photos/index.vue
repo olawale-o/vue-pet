2664d9a279376c9cbf71c0af787eb1d813f9727f
olawale-o <omoogunolawale@gmail.com>
2022-05-10 01:29:36 +0100

    Modify props

--- src/views/Pet/Photos/index.vue
+++ src/views/Pet/Photos/index.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
 import { storeToRefs } from "pinia";
 import usePetStore from "@/store/pet";
 import PhotoList from "@/components/Pet/PhotoList";
@@ -12,7 +14,10 @@ export default {
     PhotoList,
   },
   setup() {
-    const { photos } = storeToRefs(usePetStore());
+    const route = useRoute();
+    const photos = ref();
+    const { myPets } = storeToRefs(usePetStore());
+    photos.value = myPets.value[String(route.params.petId)].images;
     return {
       photos,
     };
