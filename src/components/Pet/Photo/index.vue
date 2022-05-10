128204b9cca358a0971ec13a52c9ba2cba0c7f23
olawale-o <omoogunolawale@gmail.com>
2022-05-10 01:27:05 +0100

    Create Photo component

---
+++ src/components/Pet/Photo/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div className="photo">
+    <img :src="`${BASE_URI}${photo.url}`" alt="dog" />
+    <button type="button" className="photo__btn">
+      <font-awesome-icon icon="pen" class="icon" color="#fff" />
+    </button>
+  </div>
+</template>
+
+<script>
+import BASE_URI from "@/constants";
+import { computed } from "vue";
+import { storeToRefs } from "pinia";
+import usePetStore from "@/store/pet";
+export default {
+  name: "Photo",
+  props: {
+    photoId: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const { photos } = storeToRefs(usePetStore());
+    const photo = computed(() => photos.value[String(props.photoId)]);
+    return {
+      BASE_URI,
+      photo,
+    };
+  },
+};
+</script>
