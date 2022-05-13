00e9de2acd4a47e3206b8b30405fb625488de086
olawale-o <omoogunolawale@gmail.com>
2022-05-13 08:28:27 +0100

    Rename PetToolTipPopUp to CustomToolTipPopUp

---
+++ src/components/Shared/CustomToolTipPopUp.vue
@@ -0,0 +1,18 @@
+<template>
+  <ToolTipPopUp>
+    <slot></slot>
+  </ToolTipPopUp>
+</template>
+
+<script>
+import { ToolTipPopUp } from "@/components/slots";
+export default {
+  name: "CustomToolTipPopUp",
+  components: {
+    ToolTipPopUp,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
