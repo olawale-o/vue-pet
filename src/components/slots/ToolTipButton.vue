5bf7000fda6cff7099813fbdc959603180ad2851
olawale-o <omoogunolawale@gmail.com>
2022-05-13 08:27:01 +0100

    Rename PetActionButton to ToolTipButton

---
+++ src/components/slots/ToolTipButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button" class="action__btn" @click="$emit('onAction')">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ToolTipButton",
+  emits: ["onAction"],
+  setup() {},
+};
+</script>
