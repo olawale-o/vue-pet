63f60743a17528c401564721d5f1d793ce00062c
olawale-o <omoogunolawale@gmail.com>
2022-05-29 16:37:52 +0100

    Export pet colors

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,6 @@ const BASE_URI = "http://localhost:3000";
 
 export { default as genders } from "./gender";
 export { default as breedTypes } from "./breed";
+export { default as petColors } from "./petColors";
 
 export default BASE_URI;
