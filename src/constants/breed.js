d18e7de6b8e87417640701b65156708b35b7b8fa
olawale-o <omoogunolawale@gmail.com>
2022-05-29 04:14:46 +0100

    Export breed and gender constants

---
+++ src/constants/breed.js
@@ -0,0 +1,17 @@
+const breedTypes = [
+  {
+    name: 'Breeder',
+    value: 'breeder',
+  },
+  {
+    name: 'Non-Breeder',
+    value: 'non-breeder',
+  },
+  {
+    name: 'Both',
+    value: 'both',
+  },
+];
+  
+export default breedTypes;
+  
---
+++ src/constants/gender.js
@@ -0,0 +1,17 @@
+const genders = [
+  {
+    name: 'Male',
+    value: 'm',
+  },
+  {
+    name: 'Female',
+    value: 'f',
+  },
+  {
+    name: 'Both',
+    value: 'b',
+  },
+];
+  
+export default genders;
+  
