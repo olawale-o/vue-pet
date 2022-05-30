be5353160d7a9e2ec8ffd265cc0b18aaed8ed5af
olawale-o <omoogunolawale@gmail.com>
2022-05-30 01:32:32 +0100

    Pass petIds as props to PetListing component

--- src/views/Listings/index.vue
+++ src/views/Listings/index.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <SideSearchPanel />
+    <PetListing :petIds="petIds" />
   </div>
 </template>
 
 <script>
 import { provide, reactive, onMounted } from "vue";
 import { useRouter } from "vue-router";
+import usePetStore from "@/store/pet";
+import { storeToRefs } from "pinia";
 import { genders, breedTypes, petColors } from "@/constants";
 import SideSearchPanel from "@/components/SideSearchPanel";
+import PetListing from "@/components/PetListing";
 export default {
   name: "ListingView",
   components: {
     SideSearchPanel,
+    PetListing,
   },
   setup() {
     const router = useRouter();
@@ -54,6 +59,8 @@ export default {
       });
     };
 
+    const { petIds } = storeToRefs(usePetStore());
+
     onMounted(() => {
       router.replace({
         path: "/listing",
@@ -74,7 +81,9 @@ export default {
       onGenderSelected,
       onBreedSelected,
     });
-    return {};
+    return {
+      petIds,
+    };
   },
 };
 </script>
