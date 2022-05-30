3b565aaa6ab92daa43557fdaa2820723f7ba958d
olawale-o <omoogunolawale@gmail.com>
2022-05-30 01:33:14 +0100

    Display all pets

---
+++ src/components/ListingPetCard/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="listing__dog">
+    <router-link class="link-overlay" :to="`listings/${pet.id}`">
+      {{ " " }}
+    </router-link>
+    <div class="inner">
+      <div class="img">
+        <img :src="`${BASE_URI}${pet.pic_url}`" alt="dog" :title="pet.name" />
+        <div class="media">
+          <div class="camera">
+            <span class="icon mx-2" title="media">
+            </span>
+            <span>{{ pet.images.length }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="inner__details">
+        <h1>
+          <span class="pet_name">
+            {{ titlelize(pet.name) }}
+          </span>
+          {{ " " }}
+          <router-link class="owner_name" to="/#">
+            @
+            {{ titlelize(pet.owner) }}
+          </router-link>
+        </h1>
+        <div class="middle">
+          <span class="for">BREEDER</span>
+          <p>
+            <span class="icon">
+            </span>
+            <span>Lagos</span>
+          </p>
+        </div>
+        <p class="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Lorem ipsum dolor.
+        </p>
+
+        <div class="bottom">
+          <span class="tag">{{ titlelize(pet.name) }}</span>
+          <div class="dot" />
+          <span class="tag">{{ GENDER_ENUM[pet.gender] }}</span>
+        </div>
+      </div>
+      <div class="interaction">
+        <span>$ 4000</span>
+        <button type="button" class="interaction__btn" title="like">
+          <span class="icon" title="like">
+          </span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BASE_URI from "@/constants";
+import { computed } from "vue";
+import usePetStore from "@/store/pet";
+import { storeToRefs } from "pinia";
+import { titlelize, GENDER_ENUM } from "@/helper";
+export default {
+  name: "ListingPetCard",
+  props: {
+    petId: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const { allPets } = storeToRefs(usePetStore());
+    const pet = computed(() => allPets.value[String(props.petId)]);
+
+    return {
+      pet,
+      titlelize,
+      GENDER_ENUM,
+      BASE_URI,
+    }
+  },
+}
+</script>
+
+<style scoped>
+@import "./style.css";
+</style>
---
+++ src/components/PetListing/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="listings">
+    <div class="listing__pet">
+      <ListingPetCard v-for="(petId, key) in petIds" :key="key"  :petId="petId"/>
+      <div class="pagination">
+        <div class="pagination__buttons">
+          <button
+            type="button"
+            title="prev"
+            class="button pagination__button prev"
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            title="next"
+            class="button pagination__button next"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ListingPetCard from "@/components/ListingPetCard";
+export default {
+  name: "PetListing",
+  components: {
+    ListingPetCard,
+  },
+  props: {
+    petIds: {
+      type: Array,
+    },
+  },
+  setup() { },
+}
+</script>
+
+<style scoped>
+@import "./style.css";
+</style>
